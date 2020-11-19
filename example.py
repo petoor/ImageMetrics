6866e0190e648f693e74cbb656f78e57adb3aa2d
Peter Jensen <peter.jensen@cellari.io>
2020-11-19 14:23:10 +0100

    initial commit containing binary metrics

---
+++ example.py
@@ -0,0 +1,18 @@
+import numpy as np 
+from PIL import Image
+from metrics.binary_metrics import BinaryImageMetrics
+
+base_img = np.uint8(Image.open("./test_images/base.png").convert("L"))
+expanded_img = np.uint8(Image.open("./test_images/expanded.png").convert("L"))
+missing_img = np.uint8(Image.open("./test_images/missing.png").convert("L"))
+empty = np.zeros_like(base_img)
+
+metrics = BinaryImageMetrics(base_img, missing_img)
+count = metrics.get_count()
+print(f"Count ratio is: {count}")
+f1 = metrics.get_f1()
+print(f"F1 score is : {f1}")
+f1_obj = metrics.get_f1_obj()
+print(f"F1 object score is : {f1_obj}")
+hausdorff_obj_distance = metrics.get_hausdorff_obj_distance()
+print(f"Hausdroff object distance score is : {hausdorff_obj_distance}")
